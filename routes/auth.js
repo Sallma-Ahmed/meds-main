7fee0835a3f12f74639e9377f37810a4ab821347
lookkaaaaaa <salmaahmed1660@gmail.com>
2024-04-27 18:56:01 +0300

    logout edited

--- routes/auth.js
+++ routes/auth.js
@@ -46,21 +46,18 @@ router.post("/login", async (req, res) => {
   }
 });
 //logout
-router.post("/logout",
-    async (req, res) => {
-        try {
-            req.session.destroy((err)=>{
-            })
-            if (err){
-              console.error("error destroy session ", err);
-              return res.status(500).json({ message: "internal server error"});
-            }
-            res.status(200).json({message:"logout successful"});
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ err: err });
-
-        }
-
+router.post("/logout", async (req, res) => {
+  try {
+    req.session.destroy((err) => {
+      if (err) {
+        console.error("Error destroying session:", err);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+      res.status(200).json({ message: "Logout successful" });
     });
+  } catch (err) {
+    console.error("Error in logout route:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 module.exports = router;
